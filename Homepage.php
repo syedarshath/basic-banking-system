497661cd3ba6df7049fd766f827eb9c21741e761
syedarshath <54344709+syedarshath@users.noreply.github.com>
2021-06-21 18:25:57 +0530

    Add files via upload

---
+++ Homepage.php
@@ -0,0 +1,33 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Banking System</title>
+  </head>
+
+  <body style="background-color: #ccccff;">
+  <header>
+  <?php
+  include 'navbar.php';
+  ?></header>
+<main>
+  <style>
+  body{
+    background-image: url('download (1).jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: 500px 300px;
+    width: 100%;
+    background-position: 500px 250px;
+  }
+</style>
+
+<img src="icon.png" style="width:210px; height:180px;">
+<h5 style = "position:relative; left:0px; top:80px; color:white;  font-family: Comic Sans MS;">WE ARE THERE FOR YOU</h1>
+    </main>
+  </body>
+</html>
